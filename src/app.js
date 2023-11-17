353164cf713b71201fc3ec7a17a790ee74bc0ad6
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-17 17:38:43 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+class Blog {
+  // Progression 1: Add 2 methods - addTitle(), and addDescription()
+}
+
+// Progression 2: Setup an event listner - add two functions
+// 1. `helperAddPost()`
+// 2. `helperPost()`
